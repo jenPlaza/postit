a51dbd274933f2eacb139302a3e1b9cbf94ac6c0
jenPlaza <jpplaza@student.fullsail.edu>
2021-02-26 22:07:06 -0500

    commiting changes

---
+++ Messages.js
@@ -0,0 +1,130 @@
+import React from 'react';
+//Material UI
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Paper from "@material-ui/core/Paper";
+
+//color theme
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+	typography: {
+    	fontFamily: 'Lora',
+	},
+	palette: {
+		primary: {
+		light: '#B9B9B9',
+		main: '#000000',
+		},
+		secondary: {
+		main: '#ea7a0f',
+		contrastText: '#ffffff',
+		},
+	},
+});
+
+//Smart Component
+class Messages extends React.Component {
+	//declaring state and new object
+	state ={
+		messageListing: []
+	}
+
+//function to remove item
+	removeItem = key => {
+		let messageL = this.state.messageListing
+		this.state.messageListing.splice(key,1)
+		this.setState({messageListing: this.state.messageListing})
+		localStorage.setItem('messageListing', JSON.stringify(messageL))
+	}
+	
+	componentDidMount(){
+	this.fetchData();
+}
+//fetch Api data and map json results in a list format
+	fetchData(){
+<<<<<<< HEAD:my-app/src/pages/Messages.js
+	fetch('https://randomuser.me/api/0.8/?results=10&nat=us')
+		.then(results =>{
+		return results.json();
+	}).then(data =>{
+		let mList = data.results.map((use)=>{
+			return(
+				  <li key={this.props.id} style={styles.list}>
+				<Paper variant="outlined">
+				<span>
+					<DeleteIcon style={styles.delete} size={30} onClick={this.props.dlt}/>
+					<EditIcon style={styles.edit} size={30}/>
+				</span>
+				<span key={use.results}>
+				<p style={styles.p}><b>Username:</b> {use.user.username}<br /><b>Email:</b> {use.user.email}<br/><b>Date:</b> {use.user.registered} - <b>Time:</b>{use.user.dob}<br /><br />{use.user.sha256}</p>
+				</span>
+				</Paper>
+				</li>
+			)
+=======
+		fetch('https://randomuser.me/api/0.8/?results=10&nat=us')
+			.then(results =>{
+			return results.json();
+		}).then(data =>{
+			let mList = data.results.map((use)=>{
+				return(
+					//This should be a seperate component.
+					<li key={this.props.id} style={styles.list}>
+						<Paper variant="outlined">
+							<span>
+								<DeleteIcon style={styles.delete} size={30} onClick={this.props.dlt}/>
+								<EditIcon style={styles.edit} size={30}/>
+							</span>
+							<span key={use.results}>
+								<p style={styles.p}>
+									{/* You should not use inline style tags. */}
+									<b>Username:</b> {use.user.username}
+									<b>Email:</b> {use.user.email}
+									<b>Date:</b> {use.user.registered} - <b>Time:</b>{use.user.dob}
+									{/* You should pass this through props, then use the image tag and require.*/}
+									<img alt="Messages" src="{use.user.sha256}" />
+								</p>
+							</span>
+						</Paper>
+					</li>
+				)
+			})
+			this.setState({message:mList});
+>>>>>>> 64e5b3e3b69646bc0c3b84eb8cabbbad10bb21a6:src/pages/Messages.js
+		})
+	}
+	render() { 
+		return (
+			<div>{this.state.message}</div>			
+		);
+	}
+}
+
+export default Messages
+
+const styles ={
+	list:{
+		width:'90%',
+		height:'auto',
+		padding:'2%',
+		listStyleType:'none',
+		marginTop:'3%',
+		marginLeft:'1%',
+		marginBottom:'3%',
+		background:'#B9B9B9',
+		borderRadius:'3px',
+		textAlign:'left',
+		wordWrap:'break-word',
+	},
+	delete:{
+		float:'right',
+		color:theme.palette.secondary.main,
+	},
+	edit:{
+		float:'right',
+	},
+	p:{
+		padding:'2%',
+	}
+}
--- my-app/src/pages/Watch.js
+++ my-app/src/pages/Watch.js
@@ -1,35 +1,35 @@
 // JavaScript Document
 import React from 'react';
-import YouTube from '../components/thirdparty/YouTube';
+import YouTube from '../thirdparty/YouTube';
 
 //Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 //style
-const useStyles = makeStyles(theme => ({
-	video:{
-		marginTop:'3%',
-		marginBottom:'3%',
-	}
+const useStyles = makeStyles((theme) => ({
+  video: {
+    marginTop: '3%',
+    marginBottom: '3%',
+  },
 }));
 
 //exporting class watch
 export default function Watch() {
-	const classes = useStyles();
-	return (
-		<div>
-			<Grid container>
-				<Grid item xs={12} className={classes.video}>
-					<YouTube videoId='0ujjBmVkYhg' />
-				</Grid>
-				<Grid item xs={12} className={classes.video}>
-					<YouTube videoId='sioZd3AxmnE' />
-				</Grid>
-				<Grid item xs={12} className={classes.video}>
-					<YouTube videoId='ab3Sy-HnUB0' />
-				</Grid>
-			</Grid>
-		</div>
-    );
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12} className={classes.video}>
+          <YouTube videoId="0ujjBmVkYhg" />
+        </Grid>
+        <Grid item xs={12} className={classes.video}>
+          <YouTube videoId="sioZd3AxmnE" />
+        </Grid>
+        <Grid item xs={12} className={classes.video}>
+          <YouTube videoId="ab3Sy-HnUB0" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
